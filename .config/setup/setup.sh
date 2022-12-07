888ef96d0817a1277c7e40084648250338df77e8
vlfldr <steve@vlfldr.com>
2022-12-07 12:53:16 -0500

    Switch to COPR eww install. Custom font properties for kitty. Pared down Codium extensions

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -1,6 +1,6 @@
-! /bin/bash
+#! /bin/bash
 
-enable alias creation
+# enable alias creation
 shopt -s expand_aliases
 
 # exit on no args
@@ -38,45 +38,48 @@ dnf upgrade -y
 echo "Installing rice packages..."
 dnf install -y ImageMagick bat bluez borgbackup cava codium fish freeglut fzf git grim slurp kernel-modules-extra \
 kernel-tools hyprland kitty light lsd mpv ncmpcpp neofetch neovim network-manager-applet python3-pillow ranger \
-rpmfusion-free-release rpmfusion-nonfree-release swww sxiv wayland-logout wofi
-
-# fix wayland DPI bug with custom .desktop entry
-desktop-file-install "/home/$1/.local/share/applications/codium.desktop"
+rpmfusion-free-release rpmfusion-nonfree-release swww sxiv wayland-logout wofi eww
 
 dnf install -y ./*gotop*.rpm
 
-echo "Installing eww dependencies..."
-dnf install -y gtk3-devel gdk-pixbuf2-devel cairo-devel glib2-devel glibc-devel libgcc gtk-layer-shell-devel pango-devel
+# fix wayland DPI bug with custom .desktop entry
+desktop-file-install ".local/share/applications/codium.desktop"
 
-echo "Downloading and configuring rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+# echo "Installing eww dependencies..."
+# dnf install -y gtk3-devel gdk-pixbuf2-devel cairo-devel glib2-devel glibc-devel libgcc gtk-layer-shell-devel pango-devel
 
-echo "Downloading and building eww..."
-git clone https://github.com/elkowar/eww
-cd eww && cargo build --release --no-default-features --features=wayland
+# echo "Downloading and configuring rust..."
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source "$HOME/.cargo/env"
 
-echo "Installing eww..."
-sudo -u $1 chmod +x ./target/release/eww && mv ./target/release/eww /usr/bin/eww
+# echo "Downloading and building eww..."
+# git clone https://github.com/elkowar/eww
+# cd eww && cargo build --release --no-default-features --features=wayland
 
-echo "Cleaning up..."
-cd ..
-rm -rf ./eww "$HOME/.rustup"
+# echo "Installing eww..."
+# chmod a+x ./target/release/eww && mv ./target/release/eww /usr/bin/eww
+
+# echo "Cleaning up..."
+# cd ..
+# rm -rf ./eww "$HOME/.rustup"
 
 echo "Setting default shell to fish..."
 sudo -u "$1" chsh -s /usr/bin/fish
 
 echo "Installing Cozette font..."
 mkdir -p /usr/local/share/fonts/cozette
-mv CozetteVector.otf /usr/local/share/fonts/cozette
+mv .config/setup/CozetteVector.otf /usr/local/share/fonts/cozette
 chown -R root: /usr/local/share/fonts/cozette
 chmod 644 /usr/local/share/fonts/cozette/*
 restorecon -RF /usr/local/share/fonts/cozette
-fc-cache -v
+fc-cache -r 2&>1
+
+echo "Installing Neovim plugins..."
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 read -r -p "Install VS Codium (open source VS Code) themes & extensions? [y/n]: " input
 case $input in [yY][eE][sS]|[yY])
-    xargs -n1 codium --install-extension "rice_extensions_list.txt"
+    cat "config/setup/rice_extensions_list.txt" | xargs -n1 codium --install-extension
     ;; *)
 esac
 
@@ -85,7 +88,7 @@ case $input in [yY][eE][sS]|[yY])
     wget --content-disposition "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US"
     tar -xvf ./firefox-*.tar.bz2 --directory=/opt
     rm -rf ./firefox-*.tar.bz2
-    desktop-file-install "/home/$1/.local/share/applications/nightly.desktop"
+    desktop-file-install ".local/share/applications/nightly.desktop"
     ;; *)
 esac
 
